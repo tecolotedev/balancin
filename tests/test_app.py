f621a994f1084ceeec3f39c96df385c9969a7d45
manuelstori <manuelescobedo@HV6TYCFYPP-Manuel-Escobedo.local>
2026-08-08 02:34:24 -0600

    increase vel

--- tests/test_app.py
+++ tests/test_app.py
@@ -44,7 +44,7 @@ class MotorTestTests(unittest.TestCase):
                 correction.low_delay_seconds
                 for correction in motors.corrections
             ],
-            [0.015, 0.015],
+            [0.0075, 0.0075],
         )
         sleep.assert_called_once_with(app.MOTOR_TEST_PAUSE_SECONDS)
 
